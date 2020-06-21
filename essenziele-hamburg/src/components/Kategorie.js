d218f5f576d961748d1979f3c2f6c532863d5761
akups <appiah-lindberg@outlook.com>
2020-06-21 16:29:23 +0200

    kategorie done

--- essenziele-hamburg/src/components/Kategorie.js
+++ essenziele-hamburg/src/components/Kategorie.js
@@ -1,7 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Kategorie extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const Kategorie = () => {
+  return (
+    <div>
+      <h2>Kategorie - FILTER</h2>
+      <ul>
+        <li>
+          <input type="radio" value="Alles" name="radio" id="radio1" />
+          <label for="radio1">Alles</label>
+        </li>
+        <li>
+          <input type="radio" value="Burger" name="radio" id="radio2" />
+          <label for="radio2">Burger</label>
+        </li>
+        <li>
+          <input type="radio" value="Pizza/Pasta" name="radio" id="radio3" />
+          <label for="radio3">Pizza/Pasta</label>
+        </li>
+        <li>
+          <input type="radio" value="Asiatisch" name="radio" id="radio1" />
+          <label for="radio1">Asiatisch</label>
+        </li>
+        <li>
+          <input type="radio" value="Hausmannskos" name="radio" id="radio3" />
+          <label for="radio3">Hausmannskost</label>
+        </li>
+        <li>
+          <input type="radio" value="Sonstiges" name="radio" id="radio3" />
+          <label for="radio3">Sonstiges</label>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Kategorie;
